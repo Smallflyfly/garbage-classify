8b143212111ffabfdff3a478e707707246b1b5f2
fangpf <452639095@qq.com>
2021-05-06 18:26:21 +0800

    commit

--- dataset.py
+++ dataset.py
@@ -4,3 +4,45 @@
 @author:fangpf
 @time: 2021/05/06
 """
+
+from torch.utils.data import Dataset
+import os
+from torchvision.transforms import transforms
+
+
+class GarbageDataset(Dataset):
+    def __init__(self, data_root, mode='train', image_path='', train_test_list=''):
+        super(GarbageDataset, self).__init__()
+        self.mode = mode
+        self.image_path = image_path
+        self.train_test_list = train_test_list
+        self.data_root = data_root
+        self.images = []
+        self.labels = []
+        self.transforms = transforms.Compose([
+            transforms.Resize([256, 256]),
+            transforms.RandomRotation(30),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([], [])
+        ]) if mode == 'train' else transforms.Compose([
+            transforms.Resize([256, 256]),
+            transforms.ToTensor(),
+            transforms.Normalize([], [])
+        ])
+
+    def prepare_data(self):
+        if not os.path.exists(os.path.join(self.data_root, self.train_test_list)):
+            print("train or test list file not exist")
+        with open(os.path.join(self.data_root, self.train_test_list), 'rb') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split()
+                image = os.path.join(self.data_root, line[0])
+                label = int(line[1])
+                self.images.append(image)
+                self.labels.append(label)
+
+    def __getitem__(self, index):
+        r
--- train.py
+++ train.py
@@ -4,3 +4,28 @@
 @author:fangpf
 @time: 2021/05/06
 """
+
+import argparse
+from torchvision.models import resnet50
+
+from utils.utils import load_pretrained_weights
+
+parser = argparse.ArgumentParser(description="garbage classify")
+parser.add_argument('--epoch', default=50, type=int, help='train epoch')
+parser.add_argument('--batch_size', default=16, type=int, help='train batch size')
+parser.add_argument('--weight', default='', type=str, help='weight file')
+args = parser.parse_args()
+
+epochs = args.epoch
+batch_size = args.batch_size
+pretrained_weight = args.weight
+
+
+def train():
+    net = resnet50(num_classes=40)
+    load_pretrained_weights(net, pretrained_weight)
+    dataset =
+
+
+if __name__ == '__main__':
+    train()
